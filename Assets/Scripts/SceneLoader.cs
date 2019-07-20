6ad50d9ff71ac6752d03437f3a55247bf50bfa98
Shaun Foxcroft <sfoxcroft@outlook.com>
2019-07-20 18:45:42 +0100

    Created /scripts & added buttons - Intro scene - About scene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadIntro() { SceneManager.LoadScene(0); }
+    public void LoadGame() { SceneManager.LoadScene(1); }
+    public void LoadEnding() { SceneManager.LoadScene(2); }
+    public void LoadAbout() { SceneManager.LoadScene(3); }
+}
