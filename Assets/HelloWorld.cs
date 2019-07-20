eb56e3bf03cd866c8c265e6eea3c197731ed872e
Shaun Foxcroft <sfoxcroft@outlook.com>
2019-07-20 16:14:19 +0100

    Added out-of-bounds messages.

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,54 +7,69 @@ public class HelloWorld : MonoBehaviour
 {
     // Start is called before the first frame update
     DateTime t;
-    int count, min=1, max=1000,guesses=0,upper,lower,guess=500;
-    bool found = false;
+    int count, min=1, max=1000,guesses=0,upper,lower,guess;
+    bool gameOver = false, started = false;
     void Start()
     {
-        Debug.Log("Start() called");
         t = DateTime.Now;
+        Debug.Log("Welcome to Number Wizard, the home of the most calculating mage.");
         count = 0;
-        upper = max;
+        upper = max+1;
         lower = min;
-        print(": Select a number ("+min+"-"+max+")");
-        MakeGuess(500);
-    }
-
-    // Update is called once per frame
-    
-    void Update()
-    {
-        if (!found)
-        {
-            if (Input.GetKeyDown("up"))
-            {
-                lower = guess;
-                guesses += 1;
-                MakeGuess((upper + lower) / 2);
-            }
-            else if (Input.GetKeyDown("down"))
-            {
-                upper = guess;
-                guesses += 1;
-                MakeGuess((upper + lower) / 2);
-            }
-            else if (Input.GetKeyDown("space"))
-            {
-                found = true;
-                print("Your number was {"+guess+"} & it took me "+guesses+" attempts to find it!");
-            }
-        }
-
-        //Frame counting should always be the last thing to do after the game logic.
-        CheckFrame(false);
+        Debug.Log(": Select a number ("+min+"-"+max+") then press SPACE to begin.");
+        
     }
 
     void MakeGuess(int theGuess)
     {
         guess = theGuess;
-        print(": Is your number higher or lower than " + theGuess + "?");
-        print("Press UP for higher, DOWN for lower, SPACE for equal.");
+        Debug.Log(": Is your number higher or lower than " + theGuess + "?\n" +
+                  "Press UP for higher, DOWN for lower, SPACE for equal.");
     }
+    void Complain(bool high)
+    {
+        Debug.Log("! I can't reach your number, it is too" + (high ? "high!" : "low!"));
+        gameOver = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver && started)
+        {
+            if (Input.GetKeyDown("up"))
+            {
+                lower = guess;
+                guesses += 1;
+                if (guess < max) MakeGuess((upper + lower) / 2);
+                else Complain(true);
+            }
+            else if (Input.GetKeyDown("down"))
+            {
+                upper = guess;
+                guesses += 1;
+                if (guess > min) MakeGuess((upper + lower) / 2);
+                else Complain(false);
+            }
+            else if (Input.GetKeyDown("space"))
+            {
+                gameOver = true;
+                Debug.Log("Your number was {"+guess+"} & it took me "+guesses+" attempts to find it!");
+            }
+        }
+        if (!started)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                started = true;
+                MakeGuess(500);
+            }
+        }
+        //Frame counting should always be the last thing to do after the game logic. CheckFrame(bool log)
+        CheckFrame(false);
+    }
+
+    
 
     void CheckFrame(bool log)
     {
